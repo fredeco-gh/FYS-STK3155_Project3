37074e1a2382e232c695e4838657cc564e36fcd6
Heine Husdal <heine.elias.husdal@gmail.com>
2025-11-28 21:45:51 +0100

    Made util function to generate uniformly distributed input data

---
+++ src/utils.py
@@ -0,0 +1,6 @@
+import torch
+
+# Generates the x input data from a uniform distribution
+def generate_input_data(N_samples: int, x_lim: tuple[float, float], device: str) -> torch.Tensor:
+    x = torch.rand(N_samples, 1, device=device) * (x_lim[1] - x_lim[0]) + x_lim[0]
+    return x
